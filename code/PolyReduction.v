099b16b94315c728317598e46a90f2b8f6ed8d4c
Yan-Ci Chen <65523642+GOD1539@users.noreply.github.com>
2024-12-30 18:49:24 +0800

    Add files via upload

--- code/PolyReduction.v
+++ code/PolyReduction.v
@@ -1,31 +1,9 @@
-// Verilog implementation of a polynomial reduction module
-module PolyReduction #(parameter WIDTH = 1, parameter DEGREE = 3)(
-    output reg  [3:0] out,
-    input clk,
-    input sel,
-    input [6:0] in,
-);
+module PolyReduction(D, C);   
+output [3:0] D;
+input [3:0]  C;
 
-    // Shift registers for storing intermediate values
-    reg [WIDTH-1:0] shift_reg [0:DEGREE];
-
-    integer i;
-
-    always @(posedge clk) begin
-        if (sel == 0) begin
-            // Store input into shift registers
-            shift_reg[0] <= input_data;
-            for (i = 1; i <= DEGREE; i = i + 1) begin
-                shift_reg[i] <= shift_reg[i-1];
-            end
-        end else if (sel == 1) begin
-            // Perform subtraction for reduction
-            out <= shift_reg[0] - shift_reg[DEGREE];
-            for (i = 0; i < DEGREE; i = i + 1) begin
-                shift_reg[i] <= shift_reg[i+1];
-            end
-            shift_reg[DEGREE] <= 0;
-        end
-    end
-
+assign D[3] = C[6] - C[2];
+assign D[2] = C[5] - C[1];
+assign D[1] = C[4] - C[0];
+assign D[0] = C[3];
 endmodule
